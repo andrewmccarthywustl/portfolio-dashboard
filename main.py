da12b24e84310cf5da46d3c16a04bdff9b6bbc55
andrewmccarthywustl <a.g.mccarthy@wustl.edu>
2024-09-24 14:29:38 -0500

    testing

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from stock_portfolio_app import StockPortfolioApp
 
+
 def main():
     root = tk.Tk()
     app = StockPortfolioApp(root)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
